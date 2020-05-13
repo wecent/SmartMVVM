3d7b5c39c967a6ce0290f1376630ae499587c85a
wecent <weizuochao@foxmail.com>
2020-05-13 17:34:26 +0800

    去除手动管理RxJava释放

--- lib_common/src/main/java/com/wecent/common/base/BaseViewModel.java
+++ lib_common/src/main/java/com/wecent/common/base/BaseViewModel.java
@@ -10,37 +10,22 @@ import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.Lifecycle;
 import androidx.lifecycle.LifecycleOwner;
-import io.reactivex.disposables.CompositeDisposable;
-import io.reactivex.disposables.Disposable;
-import io.reactivex.functions.Consumer;
 
 /**
  * @desc: BaseViewModel
  * @author: wecent
  * @date: 2020/5/8
  */
-public class BaseViewModel extends AndroidViewModel implements IBaseViewModel, Consumer<Disposable> {
+public class BaseViewModel extends AndroidViewModel implements IBaseViewModel {
 
     private BaseLiveData mBaseLiveData;
     /**
      * 弱引用持有避免造成内存泄漏
      */
     private WeakReference<LifecycleOwner> mLifecycleOwner;
-    /**
-     * 管理RxJava异步操作造成的内存泄漏
-     */
-    private CompositeDisposable mCompositeDisposable;
 
     public BaseViewModel(@NonNull Application application) {
         super(application);
-        mCompositeDisposable = new CompositeDisposable();
-    }
-
-    protected void addSubscribe(Disposable disposable) {
-        if (mCompositeDisposable == null) {
-            mCompositeDisposable = new CompositeDisposable();
-        }
-        mCompositeDisposable.add(disposable);
     }
 
     /**
@@ -125,17 +110,4 @@ public class BaseViewModel extends AndroidViewModel implements IBaseViewModel, C
     public void removeRxBus() {
     }
 
-    @Override
-    protected void onCleared() {
-        super.onCleared();
-        //ViewModel销毁时会执行，同时取消所有异步任务
-        if (mCompositeDisposable != null) {
-            mCompositeDisposable.clear();
-        }
-    }
-
-    @Override
-    public void accept(Disposable disposable) throws Exception {
-        addSubscribe(disposable);
-    }
 }
